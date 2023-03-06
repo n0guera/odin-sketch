3a0842e57e48da379c1240729fbec2d947714fe5
Bruno Santiago Noguera <santiiago.noguera@gmail.com>
2023-03-06 13:30:01 -0300

    Add script to change size

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-const sizeButton = document.querySelector('#size-button');
-
 const gridContainer = document.querySelector('#grid-container');
-gridContainer.style.cssText = 'display: grid; grid-template-columns: repeat(16, 20px); grid-template-rows: repeat(16, 20px);'
+gridContainer.style.cssText = 'display: grid; height: 850px; width: 850px; grid-template-columns: repeat(16, auto); grid-template-rows: repeat(16, auto);'
 grid();
+
+const sizeButton = document.querySelector('#size-button');
+sizeButton.addEventListener('click', userSize);
 
 function grid() {
     for (let i = 0; i < 256; i++) {
@@ -11,7 +12,22 @@ function grid() {
         gridContainer.appendChild(pixels);
     }
     const gridSquares = document.querySelectorAll('.grid-square');
-    for ( let i = 0; i < gridSquares.length; i++) {
+    for (let i = 0; i < gridSquares.length; i++) {
+        gridSquares[i].addEventListener('mouseover', color);
+    }
+}
+
+function userSize() {
+    let size = prompt('Enter a new size for the grid (max is 100)');
+    gridContainer.style.cssText = `display: grid; height: 900px; width: 900px; grid-template-columns: repeat(${size}, auto); grid-template-rows: repeat(${size}, auto);`;
+    gridContainer.replaceChildren();
+    for (let i = 0; i < (size * size); i++) {
+        let pixels = document.createElement('div');
+        pixels.classList.add('grid-square');
+        gridContainer.appendChild(pixels);
+    }
+    const gridSquares = document.querySelectorAll('.grid-square');
+    for (let i = 0; i < gridSquares.length; i++) {
         gridSquares[i].addEventListener('mouseover', color);
     }
 }
