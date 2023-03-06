ab6a84fe049c9ae9a3de0e687626e961ace5713a
Bruno Santiago Noguera <santiiago.noguera@gmail.com>
2023-03-06 11:57:52 -0300

    Update script.js to change color on mouseover

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 const gridContainer = document.querySelector('#grid-container');
+grid();
 
 function grid() {
     for (let i = 0; i < 256; i++) {
-        let gridSquares = document.createElement('div');
-        gridSquares.classList.add('grid-square');
-        gridContainer.appendChild(gridSquares);
+        let pixels = document.createElement('div');
+        pixels.classList.add('grid-square');
+        gridContainer.appendChild(pixels);
+    }
+    const gridSquares = document.querySelectorAll('.grid-square');
+    for ( let i = 0; i < gridSquares.length; i++) {
+        gridSquares[i].addEventListener('mouseover', color);
     }
 }
-grid();
+
+function color() {
+    this.style.cssText = 'background-color: black';
+}
